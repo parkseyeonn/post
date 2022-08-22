a9684fe41f0649bb685d6c3fb18e92b734d6efa1
parkseyeonn <yondu0902@gmail.com>
2022-08-22 21:41:37 +0900

    post style ing

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import { useRouter } from 'next/router'
 import { useState } from "react";
+import { Title } from "../../styles/pages/post/id";
 
 const Post: NextPage = () => {
   const {query} = useRouter();
   
   return (
     <>
+    <Title>제목</Title>
+    
     post {query.id}
     </>
   )
---
+++ styles/pages/post/id.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Title = styled.h3`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const Contents = styled.p`
+
+`;
+
+const CommentList = styled.ul`
+
+`;
+
+const Comment = styled.li`
+
+`;
+
+const UserProfile = styled.div`
+
+`;
+
+export {
+  Title,
+  Contents,
+}
+
